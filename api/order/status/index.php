f4340702718838bc5d25b1dde37fbd8568c9a643
Gustavo Troisi <gustavotroisi@gmail.com>
2020-06-26 15:53:21 -0300

    ajustes

--- api/order/status/index.php
+++ api/order/status/index.php
@@ -9,7 +9,7 @@ global $access_token,$collector_id;
 
 $external_reference = $_REQUEST["external_reference"];
 
-$url = "https://api.mercadopago.com/merchant_orders";
+$url = "https://api.mercadopago.com/merchant_orders/".$external_reference."?access_token=".$access_token;
  
  // REVISA AQUÍ:
  // Qué método y endpoint de la API de Mercado Pago deberías poner aquí para poder hacer un get 
@@ -19,6 +19,6 @@ $url = "https://api.mercadopago.com/merchant_orders";
  
  //https://www.mercadopago.cl/developers/es/reference/merchant_orders/_merchant_orders_id/get/
 
-curl_call("GET", $url."/".$external_reference."?access_token=".$access_token);
+curl_call("get", $url);
 
 ?>
