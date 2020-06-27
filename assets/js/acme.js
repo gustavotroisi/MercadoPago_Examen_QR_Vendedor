b70d8817a79ee5e187026c8c88d82d299c2bcd46
Gustavo Troisi <gustavotroisi@gmail.com>
2020-06-27 12:17:42 -0300

    log

--- assets/js/acme.js
+++ assets/js/acme.js
@@ -118,7 +118,7 @@ $(document).ready(function() {
 
 									//var orderStatus = elements[totalElements-1].status;
 									var orderStatus =data.status;
-									console.log('ELEMENTO: '+elements);
+									console.log('ELEMENTO: '+elements[totalElements-1]);
 									
 									$('#orderStatus').text(orderStatus);
 									$('#loading').html("<img src='assets/img/ajax-loader.gif'>");
